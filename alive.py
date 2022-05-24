5c9d7c1100f17e302a85c2a94ed861a905f9ca2c
RuwinSankapala <106127185+RuwinSankapala@users.noreply.github.com>
2022-05-24 08:32:26 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,22 +1,22 @@
-from time import sleep
+from time import sleep as snooze
 from requests import get as requestget
-from os import environ
+from os import environ as environment
 from logging import error as loggingerror
 
-BASE_URL = environ.get('BASE_URL_OF_BOT', None)
+BASE_URL = environment.get('BASE_URL_OF_BOT', None)
 try:
     if len(BASE_URL) == 0:
         raise TypeError
     BASE_URL = BASE_URL.rstrip("/")
 except TypeError:
     BASE_URL = None
-PORT = environ.get('PORT', None)
+PORT = environment.get('PORT', None)
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
             requestget(BASE_URL).status_code
-            sleep(600)
+            snooze(600)
         except Exception as e:
             loggingerror(f"alive.py: {e}")
-            sleep(2)
+            snooze(2)
             continue
